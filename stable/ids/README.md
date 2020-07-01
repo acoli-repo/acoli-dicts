afc37fc62847f8f6bfa69e0300b8f86af724c009
Christian Chiarcos <christian.chiarcos@web.de>
2020-07-02 00:10:38 +0200

    IDS converter (preliminary)

---
+++ stable/ids/README.md
@@ -0,0 +1,17 @@
+# Intercontinental Dictionary Series (IDS)
+
+IDS edited by Key, Mary Ritchie & Comrie, Bernard, version of 2018 (https://cdstar.shh.mpg.de/bitstreams/EAEA0-5F01-8AAF-CDED-0/ids_dataset.cldf.zip)
+licensed under a Creative Commons Attribution 4.0 International License. 
+
+## TODO
+- map ISO 693-3 codes to BCP47
+- TIAD-TSV export
+- splitting into individual language pairs
+
+## history
+- 2020-07-02 initial OntoLex conversion (CC)
+- 2018 original publication, CSV format (MRK & BK)
+
+## contributors
+- MRK & BK Mary Ritchie Key and Bernard Comrie (editors), for the IDS authors
+- CC Christian Chiarcos
--- stable/ids/build.sh
+++ stable/ids/build.sh
@@ -1,51 +1,111 @@
 #!/bin/bash
 # retrieves up-to-date intercontinental dictionary series data
 # for refresh, delete the src folder
-# NOTE: requires ruby 
 
-# config
-if whereis gem | grep '/' >&/dev/null; then 
+SRC=https://ids.clld.org/download
 
-#	if whereis rdf | grep '/' >& /dev/null; then echo >& /dev/null;
-#	else echo install rdf-tabular '(admin mode required)' >&/dev/null;
-		gem 'rdf-tabular', '~> 3.1'
-		gem install linkeddata 
-		gem install rdf-tabular
-#	fi;
+##########
+# CONFIG #
+##########
+# adjust to your system
 
-	# retrieve src
-	if [ -e src ]; then
-		echo found src, skipping retrieval 1>&2
-	else
-		mkdir src;
-		cd src;
-		wget -O - https://ids.clld.org/download > index.html;
-		URL=`
-			cat index.html | \
-			perl -pe 's/\s+/ /g; s/</\n</g; s/>/>\n/g;' | \
-			egrep '<a[ \t]'| \
-			sed -e s/'\s'/'\n'/g | grep 'href=' | \
-			sed -e s/'"'/'\n'/g | egrep 'zip$'`;
-		echo retrieve $URL 1>&2;
-		wget $URL;
-		cd ..
-	fi;
+# set to your TARQL installation (and/or install from https://github.com/tarql/tarql/releases)
+TARQL=tarql
 
-	# csv2rdf conversion
-	cd src;
-	for SRC in `find | egrep 'zip$'`; do
-		echo processing $SRC 1>&2;
-		mkdir $SRC.files
-		cd $SRC.files;
-		unzip ../$SRC;
-		for file in *.csv; do
-			rdf serialize $file --output-format ntriples
-			rdf tabular-json --input-format tabular $file;
-		done;
-		cd ..
-	done;
-	cd ..
+# Apache Jena installation, set to ARQ executable
+ARQ=arq
 
+############
+# RETRIEVE #
+############
+
+if [ -e src ]; then
+	echo found src, skipping retrieval 1>&2
 else
-	echo please install ruby, gem and ruby-dev 1>&2;
+	mkdir src;
+	cd src;
+	wget -O - $SRC > index.html;
+	URL=`
+		cat index.html | \
+		perl -pe 's/\s+/ /g; s/</\n</g; s/>/>\n/g;' | \
+		egrep '<a[ \t]'| \
+		sed -e s/'\s'/'\n'/g | grep 'href=' | \
+		sed -e s/'"'/'\n'/g | egrep 'zip$'`;
+	echo retrieve $URL 1>&2;
+	wget $URL;
+	if echo $URL | grep 'zip$' >&/dev/null; then 
+		unzip $URL;
+	fi;
+	cd ..
 fi;
+
+# check also
+# https://github.com/concepticon/concepticon-data/archive/v2.3.0.zip for WordNet links
+
+###########
+# CSV2RDF #
+###########
+if [ -e ids-reduced.ttl.gz ]; then
+	gunzip -c ids-reduced.ttl.gz
+else
+	if [ -e ids.ttl.gz ]; then
+		gunzip -c ids.ttl.gz;
+	else
+		(
+			# prefixes
+			cat header.ttl;
+
+			# lexicon metadata
+			echo -n '<'$SRC'> a lime:Lexicon '
+			egrep 'dc:|dcat:' src/Wordlist-metadata.json | grep -v 'conformsTo' | \
+			perl -pe '
+				s/[\r\n]//g;
+				s/^\s*"([^"]+)"\s*:\s*/;\n   \1 /g;
+				s/,\s*//g;
+			';
+			echo " .";
+
+			# individual tables
+			cd src;
+			for file in *csv; do
+				sparql=../`echo $file | sed s/'\.csv$'//`.sparql;
+				if [ -e $sparql ]; then
+					tarql $sparql $file
+				fi;
+			done;
+			cd .. >&/dev/null
+
+			# jq '.tables[]|select(.url=="forms.csv").tableSchema[]|values[].propertyUrl' src/Wordlist-metadata.json
+			# jq '.tables[]|select(.url=="forms.csv").tableSchema[]|keys' src/Wordlist-metadata.json
+			
+			#####################
+			# TODO: BCP47 codes # (instead of ISO639-3)
+			#####################
+			
+		) | \
+		tee ids.ttl;
+		gzip ids.ttl
+	fi | \
+	\
+	###########
+	# pruning #
+	###########
+	\
+	# conversion to ntriples
+	rapper -i turtle - $SRC'#' | \
+	\
+	# prune
+	egrep 'http://cldf.clld.org/v1.0/terms.rdf#(name|parameterReference|form|alt_form|alt_transcription|languageReference|glottocode|iso639P3code)|http://purl.org/dc/(elements/1.1/|terms/)|http://www.w3.org/ns/lemon/' | tee ids-reduced.ttl;
+	gzip ids-reduced.ttl
+fi | \
+\
+#################
+# OntoLex-Lemon # (single file)
+#################
+ARQ --data=- --query=ids2ontolex.sparql > ids.tsv
+# fails with out of memory exception under ARQ and BlazeGraph
+
+############
+# TIAD-TSV #
+############
+# TODO
